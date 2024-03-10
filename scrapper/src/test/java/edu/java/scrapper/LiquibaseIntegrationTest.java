8780a6effbe0dfbe4e1b78e383c9484bc23cd14f
Ayazkins <10a-yahinayaz@mail.ru>
2024-03-10 16:11:17 +0300

    tests added

--- scrapper/src/test/java/edu/java/scrapper/LiquibaseIntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/LiquibaseIntegrationTest.java
@@ -5,6 +5,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -15,7 +16,17 @@ import static org.junit.Assert.assertTrue;
 @Testcontainers
 public class LiquibaseIntegrationTest extends IntegrationTest{
     @Test
-    public void migrationTest() {
+    public void migrationTest() throws SQLException {
         assertTrue(POSTGRES.isRunning());
+        Connection connection = POSTGRES.createConnection("");
+
+        PreparedStatement statement = connection.prepareStatement("select * from chat");
+        assertThat(statement.executeQuery().getMetaData().getTableName(1)).isEqualTo("chat");
+
+        statement = connection.prepareStatement("select * from link");
+        assertThat(statement.executeQuery().getMetaData().getTableName(1)).isEqualTo("link");
+
+        statement = connection.prepareStatement("select * from chat_link");
+        assertThat(statement.executeQuery().getMetaData().getTableName(1)).isEqualTo("chat_link");
     }
 }

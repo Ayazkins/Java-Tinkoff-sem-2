b99e875310c9add9c585cdd12f4dfa12dc4af6e3
Ayazkins <10a-yahinayaz@mail.ru>
2024-03-24 19:42:08 +0300

    build fixed 3

--- scrapper/src/test/java/edu/java/controllers/LinksControllerTests.java
+++ scrapper/src/test/java/edu/java/controllers/LinksControllerTests.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -26,9 +27,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(LinksController.class)
 public class LinksControllerTests {
 
     @Autowired

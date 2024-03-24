613b157834d8aa1ab124793018f4370bbce1fb59
Ayazkins <10a-yahinayaz@mail.ru>
2024-03-24 19:33:36 +0300

    build fixed

--- scrapper/src/test/java/edu/java/controllers/LinksControllerTests.java
+++ scrapper/src/test/java/edu/java/controllers/LinksControllerTests.java
@@ -3,15 +3,24 @@ package edu.java.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.java.requests.AddLinkRequest;
 import edu.java.requests.RemoveLinkRequest;
+import edu.java.responses.LinkResponse;
+import edu.java.service.LinkService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -22,6 +31,15 @@ public class LinksControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @MockBean
+    private LinkService linkService;
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
 
     @Test
     public void testGetLinks() throws Exception {
@@ -33,6 +51,7 @@ public class LinksControllerTests {
 
     @Test
     public void testAddLink() throws Exception {
+        when(linkService.add(anyLong(), new AddLinkRequest(anyString()))).thenReturn(new LinkResponse(1L, "http://example.com"));
         AddLinkRequest request = new AddLinkRequest("http://example.com");
         mockMvc.perform(post("/links")
                 .header("chatId", 12345)
@@ -43,6 +62,7 @@ public class LinksControllerTests {
 
     @Test
     public void testDeleteLink() throws Exception {
+        when(linkService.add(anyLong(), new AddLinkRequest(anyString()))).thenReturn(new LinkResponse(1L, "http://example.com"));
         RemoveLinkRequest request = new RemoveLinkRequest("http://example.com");
         mockMvc.perform(delete("/links")
                 .header("chatId", 12345)

0883fa8f399e776dd4f4c1154ea9b5da440d585a
Ayazkins <10a-yahinayaz@mail.ru>
2024-03-27 20:28:56 +0300

    checkstyle

--- scrapper/src/main/java/edu/java/controllers/LinksController.java
+++ scrapper/src/main/java/edu/java/controllers/LinksController.java
@@ -6,7 +6,6 @@ import edu.java.responses.LinkResponse;
 import edu.java.responses.ListLinksResponse;
 import edu.java.service.LinkService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
